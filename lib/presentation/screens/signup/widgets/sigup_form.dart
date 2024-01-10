97b46a8f8a93dd1e55accac554ca1ed2aa4b368a
Ringlex <bercik.1122@wp.pl>
2024-01-10 22:38:25 +0100

    Prepare UI for sign up screen

--- lib/presentation/screens/signup/widgets/sigup_form.dart
+++ lib/presentation/screens/signup/widgets/sigup_form.dart
@@ -1,4 +1,6 @@
 import 'package:car_wayz/export.dart';
+import 'package:car_wayz/presentation/common/app_localizations.dart';
+import 'package:car_wayz/presentation/common_ui/car_wayz_input_field.dart';
 
 class SignUpForm extends ConsumerStatefulWidget {
   const SignUpForm({super.key});
@@ -22,101 +24,89 @@ class SignUpFormState extends ConsumerState<SignUpForm> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            margin: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-            child: const Text('Join to us'),
-          ),
-          Card(
-            margin: const EdgeInsets.all(20),
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Form(
-                    key: _form,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            label: Text('Email Adress'),
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                          autocorrect: false,
-                          textCapitalization: TextCapitalization.none,
-                          validator: (value) {
-                            if (value == null ||
-                                value.trim().isEmpty ||
-                                !value.contains('@')) {
-                              return 'Please enter valid email adress.';
-                            }
-                            return null;
-                          },
-                          onSaved: (newValue) {
-                            email = newValue!;
-                          },
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            label: Text('Password'),
-                          ),
-                          obscureText: true,
-                          validator: (value) {
-                            if (value == null || value.trim().length < 6) {
-                              return 'Enter least 6 characters.';
-                            }
-                            return null;
-                          },
-                          onChanged: (value) {
-                            password = value;
-                          },
-                          onSaved: (newValue) {
-                            password = newValue!;
-                          },
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            label: Text('Confirm Password'),
-                          ),
-                          obscureText: true,
-                          validator: (value) {
-                            if (value != password) {
-                              return 'Passowrd did not match.';
-                            }
-                            return null;
-                          },
-                          onSaved: (newValue) {
-                            //password = newValue!;
-                          },
-                        ),
-                      ],
-                    )),
-              ),
-            ),
-          ),
-          Gap.medium,
-          _LoginButton(onTap: _submit),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(Insets.large),
+          child: Form(
+              key: _form,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CarWayzInputField(
+                    label: context.strings.email,
+                    validator: (value) {
+                      if (value == null ||
+                          value.trim().isEmpty ||
+                          !value.contains('@')) {
+                        return 'Please enter valid email adress.';
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      email = newValue!;
+                    },
+                  ),
+                  Gap.large,
+                  CarWayzInputField(
+                    label: context.strings.password,
+
+                    isObscureText: true,
+                    validator: (value) {
+                      if (value == null || value.trim().length < 6) {
+                        return 'Enter least 6 characters.';
+                      }
+                      return null;
+                    },
+                    // onChanged: (value) {
+                    //   password = value;
+                    // },
+                    onSaved: (newValue) {
+                      password = newValue!;
+                    },
+                  ),
+                  Gap.large,
+                  CarWayzInputField(
+                    label: context.strings.confirmPassword,
+                    isObscureText: true,
+                    validator: (value) {
+                      if (value != password) {
+                        return 'Passowrd did not match.';
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      //password = newValue!;
+                    },
+                  ),
+                ],
+              )),
+        ),
+        Gap.medium,
+        _SignUpButton(onTap: _submit),
+      ],
     );
   }
 }
 
-class _LoginButton extends StatelessWidget {
+class _SignUpButton extends StatelessWidget {
   final VoidCallback onTap;
-  const _LoginButton({
+  const _SignUpButton({
     required this.onTap,
   });
+
+  static const _buttonRadius = 30.0;
+
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       key: const Key('SignUpForm_continue_raisedButton'),
       style: ElevatedButton.styleFrom(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-        backgroundColor: const Color(0xFFFFD600),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(_buttonRadius),
+        ),
+        backgroundColor: context.palette.accentColor,
       ),
       onPressed: onTap,
       child: const Text('Sign Up'),
